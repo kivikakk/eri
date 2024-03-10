eb95ed739c7110b99401744d6193a12ee01136f7
Amelia Cuss <amelia@kivikakk.ee>
2024-03-10 22:46:47 +1100

    wip.

--- src/rtlil.zig
+++ src/rtlil.zig
@@ -1,58 +1,119 @@
 const std = @import("std");
 
 pub const Module = struct {
-    name: []const u8,
-    memories: []Memory,
-    cells: []Cell,
+    const Self = @This();
 
-    fn print(mod: Module, writer: *Writer) !void {
-        try writer.print("module {s}", .{mod.name});
+    attributes: []const Attribute = &.{},
+    name: []const u8,
+    memories: []const Memory,
+    wires: []const Wire,
+    connections: []const Connection,
+    cells: []const Cell,
+
+    fn print(self: Self, writer: *Writer) !void {
+        try writer.printAttributes(self);
+
+        try writer.print("module {s}", .{self.name});
         writer.indent += 1;
 
-        for (mod.memories) |mem| {
-            try writer.print("memory width {} size {} {s}", .{ mem.width, mem.size, mem.name });
-        }
+        for (self.memories) |memory|
+            try memory.print(writer);
 
-        for (mod.cells) |cell| {
+        for (self.wires) |wire|
+            try wire.print(writer);
+
+        for (self.connections) |connection|
+            try connection.print(writer);
+
+        for (self.cells) |cell|
             try cell.print(writer);
-        }
 
-        defer writer.indent -= 1;
+        writer.indent -= 1;
         try writer.print("end", .{});
     }
 };
 
-pub const Memory = struct {
-    width: u8,
-    size: u32,
+pub const Attribute = struct {
+    const Self = @This();
+
     name: []const u8,
+    value: Value,
+
+    pub fn print(self: Self, writer: *Writer) !void {
+        try writer.print("attribute {s} {}", .{ self.name, self.value });
+    }
+};
+
+pub const Memory = struct {
+    const Self = @This();
+
+    attributes: []const Attribute = &.{},
+    width: usize,
+    size: usize,
+    name: []const u8,
+
+    fn print(self: Self, writer: *Writer) !void {
+        try writer.printAttributes(self);
+        try writer.print("memory width {} size {} {s}", .{ self.width, self.size, self.name });
+    }
+};
+
+pub const Wire = struct {
+    const Self = @This();
+
+    const Spec = struct {
+        dir: enum { input, output, inout },
+        index: usize,
+    };
+
+    width: usize,
+    spec: ?Spec = null,
+    name: []const u8,
+
+    fn print(self: Self, writer: *Writer) !void {
+        if (self.spec) |spec| {
+            try writer.print("wire width {} {s} {} {s}", .{ self.width, @tagName(spec.dir), spec.index, self.name });
+        } else {
+            try writer.print("wire width {} {s}", .{ self.width, self.name });
+        }
+    }
 };
 
 pub const Cell = struct {
     const Self = @This();
 
+    attributes: []const Attribute = &.{},
     kind: []const u8,
     name: []const u8,
     parameters: []const Parameter,
     connections: []const Connection,
 
     fn print(self: Self, writer: *Writer) !void {
+        try writer.printAttributes(self);
+
         try writer.print("cell {s} {s}", .{ self.kind, self.name });
         writer.indent += 1;
-        for (self.parameters) |parameter| {
-            try writer.print("parameter {s} {}", .{ parameter.name, parameter.value });
-        }
-        for (self.connections) |connection| {
-            try writer.print("connect {s} {}", .{ connection.name, connection.target });
-        }
+
+        for (self.parameters) |parameter|
+            try parameter.print(writer);
+
+        for (self.connections) |connection|
+            try connection.print(writer);
+
         writer.indent -= 1;
         try writer.print("end", .{});
     }
 };
 
 pub const Parameter = struct {
+    const Self = @This();
+
     name: []const u8,
     value: Value,
+
+    pub fn print(self: Self, writer: *Writer) !void {
+        try writer.print("parameter {s} {}", .{ self.name, self.value });
+    }
 };
 
 pub const Value = union(enum) {
@@ -63,18 +124,26 @@ pub const Value = union(enum) {
     bv: Bitvector,
 
     pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-        _ = fmt;
-        _ = options;
-
         switch (self) {
             .number => |n| try std.fmt.format(writer, "{}", .{n}),
-            .string => |s| try std.fmt.format(writer, "\"{s}\"", .{s}),
-            .bv => |bv| try std.fmt.format(writer, "{}", .{bv}),
+            .string => |s| {
+                try writer.writeByte('"');
+                for (s) |c| {
+                    switch (c) {
+                        inline '"', '\\' => try writer.writeAll(&.{ '\\', c }),
+                        else => try writer.writeByte(c),
+                    }
+                }
+                try writer.writeByte('"');
+            },
+            .bv => |bv| try bv.format(fmt, options, writer),
         }
     }
 };
 
 pub const Connection = struct {
+    const Self = @This();
+
     name: []const u8,
     target: union(enum) {
         wire: struct {
@@ -84,15 +153,16 @@ pub const Connection = struct {
         constant: Bitvector,
 
         pub fn format(self: @This(), comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-            _ = fmt;
-            _ = options;
-
             switch (self) {
-                .wire => |wire| try std.fmt.format(writer, "{s} [{}:{}]", .{ wire.name, wire.range.upper, wire.range.lower }),
-                .constant => |bv| try std.fmt.format(writer, "{}", .{bv}),
+                .wire => |wire| try std.fmt.format(writer, "{s} [{}]", .{ wire.name, wire.range }),
+                .constant => |bv| try bv.format(fmt, options, writer),
             }
         }
     },
+
+    pub fn print(self: Self, writer: *Writer) !void {
+        try writer.print("connect {s} {}", .{ self.name, self.target });
+    }
 };
 
 pub const Bitvector = struct {
@@ -103,16 +173,30 @@ pub const Bitvector = struct {
     pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
         _ = fmt;
         _ = options;
+
         try std.fmt.format(writer, "{}'", .{self.bits.len});
         for (self.bits) |bit| {
-            try writer.writeAll(if (bit == 1) "1" else "0");
+            try writer.writeByte(if (bit == 1) '1' else '0');
         }
     }
 };
 
 pub const Range = struct {
+    const Self = @This();
+
     upper: usize,
     lower: usize,
+
+    pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+
+        if (self.upper == self.lower) {
+            try std.fmt.format(writer, "{}", .{self.upper});
+        } else {
+            try std.fmt.format(writer, "{}:{}", .{ self.upper, self.lower });
+        }
+    }
 };
 
 pub fn output(writer: anytype, mods: []const Module) !void {
@@ -134,9 +218,19 @@ const Writer = struct {
         return .{ .inner = inner };
     }
 
-    fn print(self: Self, comptime fmt: []const u8, args: anytype) !void {
+    fn writeIndent(self: Self) !void {
         try self.inner.writeByteNTimes(' ', self.indent * 2);
+    }
+
+    fn print(self: Self, comptime fmt: []const u8, args: anytype) !void {
+        try self.writeIndent();
         try std.fmt.format(self.inner, fmt ++ "\n", args);
+    }
+
+    fn printAttributes(self: Self, target: anytype) !void {
+        for (target.attributes) |attribute| {
+            try self.print("attribute {s} {}", .{ attribute.name, attribute.value });
+        }
     }
 };
 
@@ -297,15 +391,91 @@ const Writer = struct {
 // <<
 
 // TODO: checkAllAllocationFailures
-test "cell print" {
+
+fn expectPrint(arg: anytype, expected: []const u8) !void {
     var out = std.ArrayList(u8).init(std.testing.allocator);
     defer out.deinit();
     var w = Writer.init(out.writer().any());
 
-    const cell = Cell{
+    try arg.print(&w);
+    try std.testing.expectEqualStrings(expected, out.items);
+}
+
+test "module print" {
+    try expectPrint(Module{
+        .attributes = &.{
+            .{ .name = "\\generator", .value = .{ .string = "eri" } },
+            .{ .name = "\\top", .value = .{ .number = 1 } },
+        },
+        .name = "\\top",
+        .memories = &.{.{
+            .attributes = &.{
+                .{ .name = "\\src", .value = .{ .string = "module print test" } },
+            },
+            .width = 8,
+            .size = 71,
+            .name = "\\rom_rd",
+        }},
+        .wires = &.{
+            .{ .width = 1, .name = "\\clk" },
+            .{ .width = 1, .spec = .{ .dir = .inout, .index = 0 }, .name = "\\led_0__io" },
+        },
+        .connections = &.{.{ .name = "\\i2c_bus__busy", .target = .{
+            .wire = .{ .name = "\\led_0__o", .range = .{ .upper = 0, .lower = 0 } },
+        } }},
+        .cells = &.{.{
+            .attributes = &.{
+                .{ .name = "\\src", .value = .{ .string = "module print test again" } },
+            },
+            .kind = "$dff",
+            .name = "$31",
+            .parameters = &.{
+                .{ .name = "\\WIDTH", .value = .{ .number = 1 } },
+                .{ .name = "\\CLK_POLARITY", .value = .{ .number = 1 } },
+            },
+            .connections = &.{
+                .{ .name = "\\D", .target = .{ .wire = .{
+                    .name = "$8",
+                    .range = .{ .upper = 0, .lower = 0 },
+                } } },
+                .{ .name = "\\CLK", .target = .{ .wire = .{
+                    .name = "\\clk",
+                    .range = .{ .upper = 0, .lower = 0 },
+                } } },
+                .{ .name = "\\Q", .target = .{ .wire = .{
+                    .name = "\\w_en",
+                    .range = .{ .upper = 0, .lower = 0 },
+                } } },
+            },
+        }},
+    },
+        \\attribute \generator "eri"
+        \\attribute \top 1
+        \\module \top
+        \\  attribute \src "module print test"
+        \\  memory width 8 size 71 \rom_rd
+        \\  wire width 1 \clk
+        \\  wire width 1 inout 0 \led_0__io
+        \\  connect \i2c_bus__busy \led_0__o [0]
+        \\  attribute \src "module print test again"
+        \\  cell $dff $31
+        \\    parameter \WIDTH 1
+        \\    parameter \CLK_POLARITY 1
+        \\    connect \D $8 [0]
+        \\    connect \CLK \clk [0]
+        \\    connect \Q \w_en [0]
+        \\  end
+        \\end
+        \\
+    );
+}
+
+test "cell print" {
+    try expectPrint(Cell{
         .kind = "$memrd_v2",
         .name = "$70",
         .parameters = &.{
+            .{ .name = "\\MEMID", .value = .{ .string = "\\storage" } },
             .{ .name = "\\WIDTH", .value = .{ .number = 8 } },
             .{ .name = "\\ABITS", .value = .{ .number = 5 } },
         },
@@ -315,16 +485,14 @@ test "cell print" {
         } } }, .{ .name = "\\ARST", .target = .{ .constant = .{
             .bits = &.{0},
         } } } },
-    };
-
-    try cell.print(&w);
-    try std.testing.expectEqualStrings(
+    },
         \\cell $memrd_v2 $70
+        \\  parameter \MEMID "\\storage"
         \\  parameter \WIDTH 8
         \\  parameter \ABITS 5
         \\  connect \ADDR $signature__addr$18 [4:0]
         \\  connect \ARST 1'0
         \\end
         \\
-    , out.items);
+    );
 }
